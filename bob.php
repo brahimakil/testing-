95568346b71a19818b030a24ba82d92e220bf817
ibrahim akil <brhimakil.1@gmail.com>
2024-05-21 22:20:41 +0200

    edited bob.php for the first time in developer

--- bob.php
+++ bob.php
@@ -1,15 +1,3 @@
 <?php
-echo "kol 5ara";
-echo "knt nase semi-clomun ya hmar"
+    echo "this is developer mode i am bob "
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>semi-column</h1>
-</body>
-</html>
