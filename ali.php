4bdc56d5642a7a4af7248302317eb554d481541e
Ali shaalan <shaalanali20@gmail.com>
2024-05-21 22:22:22 +0300

    ali develop

--- ali.php
+++ ali.php
@@ -1,14 +1,4 @@
 <?php 
 echo "hello its me ali";
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=\], initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>First project </h1>
-</body>
-</html>
+echo "this is developer 2 
+i am ali";
