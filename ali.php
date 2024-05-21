bdbbdf6f5f5a5b83c53d2d2f1121cffbac43491a
Ali shaalan <shaalanali20@gmail.com>
2024-05-21 22:13:04 +0300

    adding first page

---
+++ ali.php
@@ -0,0 +1,14 @@
+<?php 
+echo "hello its me ali";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=\], initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>First project </h1>
+</body>
+</html>
