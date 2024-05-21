d0340066532b0826611ad49e5b38596c5b511795
Ali shaalan <shaalanali20@gmail.com>
2024-05-21 22:16:04 +0300

    fixing bug

--- bob.php
+++ bob.php
@@ -1,3 +1,15 @@
 <?php
-echo "kol 5ara"
+echo "kol 5ara";
+echo "knt nase semi-clomun ya hmar"
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>semi-column</h1>
+</body>
+</html>
